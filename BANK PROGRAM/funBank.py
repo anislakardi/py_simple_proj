ef8771c573ec470471b3ceda15bc94b8d1bbcb07
Lakardi Mohamed Anis <127695733+lakardi-mohamed-anis@users.noreply.github.com>
2024-06-12 12:17:17 +0100

    bank prog using modules

---
+++ BANK PROGRAM/funBank.py
@@ -0,0 +1,16 @@
+def showBalance(totalBalnce):
+    print("----- TOTAL BALANCE: -----")
+    print(f"{totalBalnce:.2f} DA")
+    print("--------------------------")
+
+def deposit():
+    print("--------------------------")
+    amont = float(input("Enter how much you want to deposit: "))
+    return amont
+
+def withdraw():
+    print("--------------------------")
+    amont = float(input("Enter how much you want to draw: "))
+    return amont
+
+
---
+++ BANK PROGRAM/mainBank.py
@@ -0,0 +1,29 @@
+import funBank
+
+
+
+print("--------------------------")
+print("WELCOME TO BANK PROGRAM")
+print("--------------------------")
+print()
+balance = 0
+while True:
+    print("**************************")
+    print("1.Show Balance")
+    print("2.Deposit")
+    print("3.Withdraw")
+    print("4.Exit")
+    print("**************************")
+    choise = input("Enter your choise (1-4): ")
+    print("**************************")
+    if choise == "4":
+        break
+    elif choise == "1":
+        funBank.showBalance(balance)
+    elif choise == "2":
+        balance += funBank.deposit()
+    elif choise == "3":
+        balance -= funBank.withdraw()
+    else:
+        print("INVALID INPUT")
+print("BYE")
