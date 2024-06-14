99897ae092936d10a955d93bd09eea58d0524ed0
Lakardi Mohamed Anis <127695733+lakardi-mohamed-anis@users.noreply.github.com>
2024-06-14 17:01:53 +0100

    fix the prblm to increment the code var

--- BANK OOP/Compte.py
+++ BANK OOP/Compte.py
@@ -7,7 +7,10 @@ class Compte:
     def __init__(self, nom, prenom):
         self.nom = nom
         self.prenom= prenom
-        self.code += self.code + 1
+        # Increment the class-level code variable
+        Compte.code += 1
+        # Assign the incremented code to the instance
+        self.code = Compte.code
 
     def verser(self, mt):
         self.solde += mt
@@ -16,7 +19,7 @@ class Compte:
         self.solde -= mt
     def afficher(self):
         print(f"********** Mr.{self.nom} {self.prenom} **********")
-        print(f"Code: {self.code}| Solde: {self.solde}")
+        print(f"Code: {self.code}| Solde: {self.solde: ,}$")
         print()
 
 
--- BANK OOP/mainCompte.py
+++ BANK OOP/mainCompte.py
@@ -1,12 +1,18 @@
 import Compte
 
 
-Compte1 = Compte.Compte("Anis", "Lakardi")
-Compte2 = Compte.Compte("Nassim", "Lakardi")
+Compte1 = Compte.Compte("Messi", "Leonel")
+Compte2 = Compte.Compte("Neymar jr", "Dasilva Santos")
+Compte3 = Compte.Compte("Ronaldo", "Cristiano")
 
-Compte1.verser(1000)
-Compte1.retirer(300)
-Compte2.verser(100)
+Compte1.verser(500000000)
+Compte1.retirer(150000)
+Compte2.verser(90000000)
+Compte2.retirer(15000000)
+Compte3.verser(450000000)
+Compte3.retirer(1000000)
 
-Compte2.afficher()
+
 Compte1.afficher()
+Compte2.afficher()
+Compte3.afficher()
